19d0476a4e8f9ee8635e3ea65bd506e831dd00f8
Alex Babko <aliaksandr.bobko@gmail.com>
2021-06-14 13:20:36 +0300

    тут ваше сообщение о коммите 3

--- test1.py
+++ test1.py
@@ -1,4 +1,4 @@
-
+#fir Git
 import time
 import math
 import os
@@ -35,8 +35,8 @@ formula_result=calc(x)
 field=browser.find_element_by_id('answer')
 field.send_keys(formula_result)
 
-button2 = browser.find_element_by_id('solve')
-button2.click()
+button8 = browser.find_element_by_id('solve')
+button8.click()
 
 #
 
